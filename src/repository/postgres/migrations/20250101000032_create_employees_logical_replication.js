d54a781960055cd2bcffe6a3b4b4b65a5883979d
abdulfalaq5 <abdulfalaq5@gmail.com>
2025-10-23 12:52:37 +0700

    Fix: add lgika duplkita postgre tabel employee

---
+++ src/repository/postgres/migrations/20250101000032_create_employees_logical_replication.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.raw('CREATE PUBLICATION sso_pub FOR TABLE employees;');
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.raw('DROP PUBLICATION IF EXISTS sso_pub;');
+};
